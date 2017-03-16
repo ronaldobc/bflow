65d7e7da0f6ecf085fa9297d62e3c35273214de7
Ronaldo Baragati Cassini <ronaldorb@hotmail.com>
2017-03-15 21:35:57 -0300

    Adicionado template basico

---
+++ resources/views/app/rodape.blade.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+    <!-- To the right -->
+    <div class="pull-right hidden-xs">
+        Versão 1.0
+    </div>
+    <!-- Default to the left -->
+    <strong>Copyright © 2017 <a href="#">RBC Soft</a>.</strong> Todos direitos reservados.
+</footer>
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,44 @@
 |
 */
 
+Route::get('/principal', ['as'=>'principal', function(){
+
+    $data['tasks'] = [
+        [
+            'name' => 'Design New Dashboard',
+            'progress' => '87',
+            'color' => 'danger'
+        ],
+        [
+            'name' => 'Create Home Page',
+            'progress' => '76',
+            'color' => 'warning'
+        ],
+        [
+            'name' => 'Some Other Task',
+            'progress' => '32',
+            'color' => 'success'
+        ],
+        [
+            'name' => 'Start Building Website',
+            'progress' => '56',
+            'color' => 'info'
+        ],
+        [
+            'name' => 'Develop an Awesome Algorithm',
+            'progress' => '10',
+            'color' => 'success'
+        ]
+    ];
+
+    return view('principal')->with($data);
+
+}] );
+
+
+
 Route::get('/', function () {
-    return view('welcome');
+
+    return view('login');
+
 });
